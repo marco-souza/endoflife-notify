46c6e4b737e824e1cfda7993bf68dc4d8ad54452
Marco Antônio <ma.souza.junior@gmail.com>
2025-04-28 13:50:06 -0300

    fea: setup subscription to versions with webhook

--- main.ts
+++ main.ts
@@ -1,78 +1,99 @@
 import { type Context, Hono } from "hono";
 import { type } from "arktype";
-
-// set subscription schema
-const SubscriptionSchema = type({
-  technology: "string",
-  version: "string",
-  days_to_expire: "0 < number.integer < 366",
-  webhook_url: "string",
-  webhook_secret: "string",
-});
-
-// setup db
-const kv = await Deno.openKv();
+import { apiClient } from "#/lib/endoflife.ts";
+import * as tg from "#/lib/telegram.ts";
+import { Subscription } from "#/entities/subscription.ts";
+import { config } from "#/config.ts";
 
 const app = new Hono();
+const kv = await Deno.openKv();
 
 app.get("/", (c) => c.text("Hello World!"));
 
-app.post("/subscribe", async (c: Context) => {
-  try {
-    // Parse and validate the request body against the SubscriptionSchema
-    const body = await c.req.json();
-    const subs = SubscriptionSchema(body);
-    if (subs instanceof type.errors) {
-      // hover summary to see validation errors
-      console.error(subs.summary);
-      return c.json({ error: "Invalid subscription data" }, 400);
-    }
+app.post("/callback", async (c) => {
+  const body = await c.req.json();
+  console.log("Received callback:", body);
 
-    console.info("Parsed subscription:", subs);
-
-    // list of all technologies
-    const registeredTechs = await kv.get<string[]>(["technologies"]);
-    if (!registeredTechs.value) {
-      return c.json({ error: "No technologies registered" }, 400);
-    }
-
-    // include the technology if it is not already registered
-    if (!registeredTechs.value.includes(subs.technology)) {
-      await kv.set(["technologies"], [
-        ...registeredTechs.value,
-        subs.technology,
-      ]);
-    }
-
-    // Store the subscription in the database
-    const subRes = await kv.set([
-      "subscriptions",
-      subs.technology,
-      subs.version,
-    ], subs);
-
-    // if not ok, return
-    if (!subRes.ok) {
-      console.error("Failed to store subscription:", subRes);
-      return c.json({ error: "Failed to store subscription" }, 500);
-    }
-
-    return c.json({ message: "Subscription created successfully" }, 201);
-  } catch (error) {
-    console.error("Error creating subscription:", error);
-    return c.json({ error: "Failed to create subscription" }, 500);
+  const subscription = Subscription(body);
+  if (subscription instanceof type.errors) {
+    // hover summary to see validation errors
+    console.error(subscription.summary);
+    return c.json({ error: "Invalid subscription data" }, 400);
   }
+
+  const { webhook_secret } = subscription;
+  if (webhook_secret != config.webhookSecrets) {
+    return c.json({ error: "Invalid webhook secret" }, 401);
+  }
+
+  // Send a message on my telegram
+  const message = `Package ${subscription.tech} ${subscription.version} is EOL`;
+
+  const res = await tg.apiClient.sendMessage(config.telegram.chatId, message);
+  console.log("Message sent to Telegram", res);
+
+  return c.json({ message });
+});
+
+app.post("/subscribe/:tech", async (c: Context) => {
+  const tech = c.req.param("tech");
+  const body = await c.req.json();
+
+  const subscription = Subscription({ tech, ...body });
+  if (subscription instanceof type.errors) {
+    // hover summary to see validation errors
+    console.error(subscription.summary);
+    return c.json({ error: "Invalid subscription data" }, 400);
+  }
+
+  const eol = await apiClient.singleCycleDetails(subscription); // TODO: cache results
+
+  // if end of live already reached, return error
+  if (eol.eol === true) {
+    return c.json(
+      { error: "Cannot subscribe to an expired version!", eol },
+      404,
+    );
+  }
+
+  const today = new Date();
+  const notifyDate = new Date(
+    eol.eol.getTime() - body.days_before_expire * 24 * 60 * 60 * 1000,
+  );
+
+  // INFO: enqueue notification
+  const delay = Number(100) ?? notifyDate.getTime() - today.getTime();
+
+  console.log(
+    `Enqueueing notification for ${subscription.tech} ${subscription.version} in ${delay}ms`,
+  );
+
+  await kv.enqueue(subscription, { delay });
 });
 
 Deno.serve(app.fetch);
 
-// register daily cronjob
-Deno.cron(
-  "Check what versions techs are expiring today",
-  "* * * * *",
-  async () => {
-    const today = new Date();
-    const subscriptions = await kv.get(["subscriptions"]);
-    console.log({ subscriptions, today });
-  },
-);
+kv.listenQueue(async (msg) => {
+  const subscription = Subscription(msg);
+  if (subscription instanceof type.errors) {
+    console.error(subscription);
+    return;
+  }
+
+  const { webhook_url } = subscription;
+  console.log(`Sending notification`, subscription);
+
+  const res = await fetch(webhook_url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(subscription),
+  });
+
+  if (!res.ok) {
+    const errorBody = await res.text();
+    console.error("Failed to send notification", errorBody);
+    return;
+  }
+
+  console.log("Notification sent successfully", subscription);
+});
