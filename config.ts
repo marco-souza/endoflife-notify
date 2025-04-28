3c58babff4cae9d5092e667bdc7c4cc59b3e76e8
Marco Antônio <ma.souza.junior@gmail.com>
2025-04-28 13:48:52 -0300

    fea: create telegram bot lib

--- config.ts
+++ config.ts
@@ -1,11 +1,21 @@
 import { type } from "arktype";
 
+const TelegramConfig = type({
+  botToken: "string",
+  chatId: "string",
+});
+
 const Config = type({
   webhookSecrets: "string",
+  telegram: TelegramConfig,
 });
 
 const maybeConfig = Config({
   webhookSecrets: Deno.env.get("WEBHOOK_SECRET") ?? "",
+  telegram: {
+    botToken: Deno.env.get("TELEGRAM_BOT_TOKEN") ?? "",
+    chatId: Deno.env.get("TELEGRAM_CHAT_ID") ?? "",
+  },
 });
 
 if (maybeConfig instanceof type.errors) {
