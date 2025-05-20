0d4bcab93d56cfec3712ca28bfc8bc8bbd8a5e3f
Marco Antônio <ma.souza.junior@gmail.com>
2025-05-20 18:37:11 -0300

    enh: create cron service

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 import { type Context, Hono } from "hono";
 import { type } from "arktype";
-import { apiClient } from "#/lib/endoflife.ts";
 import * as tg from "#/lib/telegram.ts";
 import { Subscription } from "#/entities/subscription.ts";
 import { config } from "#/config.ts";
 import { subscriptionService } from "#/services/subscription.ts";
+import { setupCronJobs } from "./services/cron.ts";
 
 const app = new Hono();
 
@@ -62,66 +62,4 @@ app.post("/subscribe/:tech", async (c: Context) => {
 
 Deno.serve(app.fetch);
 
-// cron job
-const CRON_EXPR = Deno.env.get("PROD") ? "0 0 * * *" : "* * * * *";
-Deno.cron(
-  "Dayly Checks",
-  CRON_EXPR,
-  async () => {
-    const technologies = await subscriptionService.listTechnologies();
-
-    for (const technology of technologies) {
-      const { tech, version } = technology;
-      const res = await apiClient.singleCycleDetails({ tech, version });
-      if (res.eol === true) {
-        console.log("Subscription expired", technology);
-        continue;
-      }
-
-      const { eol } = res;
-      const today = new Date();
-
-      const subscriptions = await subscriptionService
-        .getSubscriptionByTechnology(
-          tech,
-          version,
-        );
-      if (!subscriptions) {
-        console.log("No subscriptions found for technology", tech, version);
-        continue;
-      }
-
-      const toNotify = subscriptions.filter(({ days_before_expire }) => {
-        const diffInDays = Math.floor(
-          (eol.getTime() - today.getTime()) / (1000 * 60 * 60 * 24),
-        );
-        return (diffInDays <= days_before_expire);
-      });
-
-      await Promise.all(
-        toNotify.map(async (subscription) => {
-          const { webhook_url, webhook_secret } = subscription;
-          if (webhook_secret != config.webhookSecrets) {
-            console.log("Invalid webhook secret", subscription);
-            return;
-          }
-          const res = await fetch(webhook_url, {
-            method: "POST",
-            headers: {
-              "User-Agent": "eol-notify",
-              "Content-Type": "application/json",
-              "Authorization": `Bearer ${webhook_secret}`,
-            },
-            body: JSON.stringify(subscription),
-          });
-
-          if (!res.ok) {
-            console.log("Error sending notification", res.statusText);
-          } else {
-            console.log("Notification sent", res.statusText);
-          }
-        }),
-      );
-    }
-  },
-);
+setupCronJobs();
